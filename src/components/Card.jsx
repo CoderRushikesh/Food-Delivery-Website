4930bf5e679be51aaa92e17dca7a633fbff19384
CoderRushikesh <rushikamble462@gmail.com>
2025-12-16 00:24:55 +0530

    change icons for type veg / non veg

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
  import image1 from "../assets/image1.avif"
  import { LuLeafyGreen } from "react-icons/lu";
+ import { GiChickenOven } from "react-icons/gi";
+
 function Card({name , image , price , type}) {
   return (
    <div className='w-[300px] h-[400px] bg-white  rounded-lg p-3 flex flex-col gap-3 
@@ -17,7 +19,9 @@ function Card({name , image , price , type}) {
 
 <div className='w-full flex justify-between items-center'>
 <div className='text-xl font-bold text-green-500'> Rs {price}/- </div>
-<div className='flex justify-center items-center gap-2 text-green-500 text-lg font-semibold'> <LuLeafyGreen /> <span>{type}</span> </div>
+<div className='flex justify-center items-center gap-2 text-green-500 text-lg font-semibold'> 
+   {type==="veg"?<LuLeafyGreen />:<GiChickenOven className='text-red-600' />}  
+<span>{type}</span> </div>
 </div>
 
 <button className='w-full p-4 bg-green-200 rounded-lg
