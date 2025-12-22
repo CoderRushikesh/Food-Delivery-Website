cfea1e2fd13f80104bde6613733446489416eb48
CoderRushikesh <rushikamble462@gmail.com>
2025-12-22 23:22:34 +0530

    changes implemented in filter function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { MdFastfood } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
 import { LuShoppingBag } from "react-icons/lu";
+import { dataContext } from '../context/UserContext';
 
 function Nav() {
+
+  let  {input,setInput} = useContext(dataContext)
+
   return (
     <div className='w-full h-[100px] flex justify-between items-center
     px-4 md:px-5'>
@@ -16,9 +20,10 @@ flex justify-center items-center rounded-b-md  text-purple-900 shadow-xl'>
 
 
  <form className='w-[45%] h-[60px] bg-white flex items-center px-5 gap-5 
- rounded-md shadow-blue-100 shadow-md md:w-[70%]'>
+ rounded-md shadow-blue-100 shadow-md md:w-[70%]' onSubmit={(e)=>e.preventDefault()}>
 <FaSearch />
-<input type="text" placeholder='Search Items... ' className='w-full outline-none text-[12px] md:text-[12px] '/>
+<input type="text" placeholder='Search Items... ' className='w-full outline-none text-[12px] md:text-[12px]
+ ' onChange={(e)=>setInput(e.target.value)} value={input}/>
  </form>
 
   <div className=' bg-white w-[60px] h-[60px] 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,28 @@
-import React, { useState }  from "react";
+import   {React, useContext, useEffect, useState }  from "react";
 import Nav from "../components/Nav";
 import categories from "../category";
 import Card from "../components/card";
 import { food_items } from "../food";
 import Footer from "../components/Footer";
+import { dataContext } from "../context/UserContext";
 
 
 function Home(){
-    let [cate , setCate] =useState(food_items) 
+    let {input , setInput ,cate,setCate}=useContext(dataContext);
+
+    useEffect(()=>{
+     let newList =  food_items.filter((item)=> item.food_name.includes(input)
+    ||item.food_name.toLowerCase().includes(input) )
+      setCate(newList);
+    },[input])
 
  function filter(category){
    if(category==="All"){
-    setCate(food_items)
+    setCate(food_items);
    }else{
-    const newList = food_items.filter((item) =>(item.food_category===category))
+    const newList = food_items.filter((item) =>(item.food_category===category));
     // food_items.filter((items)=>( items.food_category===category))
-      setCate(newList)
+      setCate(newList);
 }
      
  }
@@ -25,26 +32,28 @@ function Home(){
         <div className="bg-slate-200 w-full min-h-screen">
 
      <Nav/>
-     
-    <div className="flex flex-wrap justify-center items-center gap-6
-    w-full]">
-        {categories.map((items)=>(
+
+     {!input?  <div className="flex flex-wrap justify-center items-center gap-6
+    w-full">
+        {categories.map((item)=>(
     
             <div className=" flex flex-col items-center-safe
             w-[120px] h-[110px]  bg-white
             p-5 font-semibold text-gray600 rounded-lg shadow-blue-100 shadow-xl hover:bg-green-200 cursor-pointer
-             transition-all duration-50  " onClick={()=> filter(items.name)}>
-                {items.image} {items.name}
+             transition-all duration-50  " onClick={()=> filter(item.name)}>
+                {item.image} {item.name}
             </div>
         ))}
-    </div>
+    </div>:null}
+     
+  
     
     <div className="w-full flex flex-wrap gap-5 px-5 justify-center 
     items-center pt-8 pb-8">
     {/* <Card/> */}
   {cate.map((item)=>{
     return(
-   <Card name={item.food_name} image={item.food_image} price={item.price}  type={item.food_type}/>
+   <Card key={item.food_name} name={item.food_name} image={item.food_image} price={item.price}  type={item.food_type}/>
     );  
 })}
 
@@ -57,3 +66,5 @@ function Home(){
 // new change 
 
 export default Home;
+
+
